abb15129eac717604988a2a3457ddc67caead04b
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2023-12-20 14:02:13 +0700

    router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { configureStore } from "@reduxjs/toolkit";
+import globalReducer from "./state/index.js";
+// import { setupListeners } from "@reduxjs/toolkit/query";
+import { Provider } from "react-redux";
 import './index.css'
+// import { api } from "state/api";
 
+
+const store = configureStore({
+  reducer: {
+    global: globalReducer,
+  },
+});
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 )
