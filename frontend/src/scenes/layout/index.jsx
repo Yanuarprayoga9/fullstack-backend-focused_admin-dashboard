dcb49057221bad20c51df224ba610bfd4cec9aeb
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2023-12-20 18:42:46 +0700

    setting up component

--- frontend/src/scenes/layout/index.jsx
+++ frontend/src/scenes/layout/index.jsx
@@ -1,7 +1,30 @@
-const Layout = () => {
-  return (
-    <div>Layout</div>
-  )
-}
+// import { useState } from "react";
+import { Box, useMediaQuery } from "@mui/material";
+import { Outlet } from "react-router-dom";
+import Navbar from "../../components/Navbar";
+import Sidebar from "../../components/Sidebar";
+import { useState } from "react";
 
-export default Layout
+const Layout = () => {
+  const isNonMobile = useMediaQuery("(min-width: 600px)");
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  return (
+    <Box display={isNonMobile ? "flex" : "block"} width="100%" height="100%">
+      <Sidebar
+        isNonMobile={isNonMobile}
+        drawerWidth="250px"
+        isSidebarOpen={isSidebarOpen}
+        setIsSidebarOpen={setIsSidebarOpen}
+      />
+      <Box flexGrow={1}>
+        <Navbar
+          isSidebarOpen={isSidebarOpen}
+          setIsSidebarOpen={setIsSidebarOpen}
+        />
+        <Outlet />
+      </Box>
+    </Box>
+  );
+};
+
+export default Layout;
