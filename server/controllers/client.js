7f1fcb3584cd9fba07f864604152f8adc972a862
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2023-12-21 15:24:05 +0700

    geo functional

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -2,38 +2,36 @@ import Product from "../models/Product.js";
 import ProductStat from "../models/ProductStat.js";
 import User from "../models/User.js";
 import Transaction from "../models/Transaction.js";
-export const getProducts = async (req,res) => {
-    try {
-        const products = await Product.find();
-        const ProductWithStat = await Promise.all(
-          products.map(async (product) => {
-            const stat = await ProductStat.find({
-              productId: product._id,
-            });
-            return {
-              ...product._doc,
-              stat,
-            };
-          })
-        );
-        
-        // Now ProductWithStat is an array of objects with product information and corresponding stats
-        
-        res.status(200).json(ProductWithStat);
-    } catch (error) {
-        res.status(404).json({ message: error.message });
-    }
+export const getProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+    const ProductWithStat = await Promise.all(
+      products.map(async (product) => {
+        const stat = await ProductStat.find({
+          productId: product._id,
+        });
+        return {
+          ...product._doc,
+          stat,
+        };
+      })
+    );
 
-}
-export const getCustomers = async (req,res) => {
-    try {
-        const customers = await User.find({role:"user"}).select("-password")    
-        res.status(200).json(customers);
-    } catch (error) {
-        res.status(404).json({ message: error.message });
-    }
+    // Now ProductWithStat is an array of objects with product information and corresponding stats
 
-}
+    res.status(200).json(ProductWithStat);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+export const getCustomers = async (req, res) => {
+  try {
+    const customers = await User.find({ role: "user" }).select("-password");
+    res.status(200).json(customers);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
 
 export const getTransactions = async (req, res) => {
   try {
@@ -70,6 +68,30 @@ export const getTransactions = async (req, res) => {
       transactions,
       total,
     });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+export const getGeography = async (req, res) => {
+  try {
+    const users = await User.find();
+
+    const mappedLocations = users.reduce((acc, { country }) => {
+      const countryISO3 = getCountryIso3(country);
+      if (!acc[countryISO3]) {
+        acc[countryISO3] = 0;
+      }
+      acc[countryISO3]++;
+      return acc;
+    }, {});
+
+    const formattedLocations = Object.entries(mappedLocations).map(
+      ([country, count]) => {
+        return { id: country, value: count };
+      }
+    );
+
+    res.status(200).json(formattedLocations);
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
